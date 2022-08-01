f75780f2cabcfc59f6e2292ff5130baaff128bbb
nsk-here <110277686+nsk-here@users.noreply.github.com>
2022-08-01 22:15:34 +0530

    Update code.java

--- 62. Unique Paths/code.java
+++ 62. Unique Paths/code.java
@@ -1,21 +1,18 @@
+
 class Solution {
-  
     public int uniquePaths(int m, int n) 
     {
       int dp[][] = new int[m+1][n+1];
-      
-      return findPath(dp,m,n);
-        
+      return findPath(dp,m,n);  
     }
   
     public static int findPath(int dp[][], int m, int n)
     {
       if(m == 1 || n==1)
-      {
         return 1;
-      }
-      if(dp[m][n] != 0)return dp[m][n];
+      if(dp[m][n] != 0)
+        return dp[m][n];
 
-      return dp[m][n] = findPath(dp,m-1,n)+findPath(dp,m,n-1)
+      return dp[m][n] = findPath(dp,m-1,n)+findPath(dp,m,n-1);
     }
 }
