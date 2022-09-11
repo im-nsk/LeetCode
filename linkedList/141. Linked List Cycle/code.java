6f0465c6f2c1580032683e3dd949befcb41e79a7
nsk-here <110277686+nsk-here@users.noreply.github.com>
2022-09-11 23:16:24 +0530

    Update code.java

--- linkedList/141. Linked List Cycle/code.java
+++ linkedList/141. Linked List Cycle/code.java
@@ -1,3 +1,8 @@
+Explanation:
+If we take one slow nd one fast pointer & one will be faster than another then if cycle existing, difinitely it will at some point.
+    example: On one circle ground track, two player are running, one is faster than another then difinitely faster one will be crossed slower one, 
+             just imagine this scenario, surely, you will understand.
+
 public class Solution {
     public boolean hasCycle(ListNode head) {
       
