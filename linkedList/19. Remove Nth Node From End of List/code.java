ad0ea9fcacb519a0696dcffee34d496e19de7794
nsk-here <110277686+nsk-here@users.noreply.github.com>
2022-09-12 22:37:44 +0530

    Update code.java

--- linkedList/19. Remove Nth Node From End of List/code.java
+++ linkedList/19. Remove Nth Node From End of List/code.java
@@ -1,3 +1,5 @@
+Explanation: No need to traverse till last node to find the length: Instead of this: take two pointer, traverse one pointer n+1 because when we reach 
+             then nth node from last, then after traversing nth time we can reach to null value. I hope you are getting point. 
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
       ListNode dummyHead = new ListNode(0);
@@ -5,6 +7,7 @@ class Solution {
       ListNode fast = dummyHead;
       ListNode slow = dummyHead;
       
+      //This traversing will maintain distance between n+1 , it means fast pointer will reach to null then slow pointer will be before the nth node from last
       for(int i = 1; i <= n+1; i++)
         fast = fast.next;
       
