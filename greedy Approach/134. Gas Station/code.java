c41859e95f878152728d8dc7f17167d76234e740
nsk-here <110277686+nsk-here@users.noreply.github.com>
2022-09-15 21:05:37 +0530

    Update code.java

--- greedy Approach/134. Gas Station/code.java
+++ greedy Approach/134. Gas Station/code.java
@@ -1,4 +1,5 @@
-Problem Link: https://leetcode.com/problems/gas-station/
+Problem Link: 
+https://leetcode.com/problems/gas-station/
 
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
@@ -8,7 +9,7 @@ class Solution {
      // traverse through each and every element
         for(int i = 0; i< gas.length; i++){
             
-          // find the remaining gas nd keep adding till last, where we find totalSum is negative value, it means, we can't move forward with that fuel, make it totalfuel 0, start with next index:   
+   // find the remaining gas nd keep adding till last, where we find totalSum is negative value, it means, we can't move forward with that fuel, make it    totalfuel 0, start with next index:   
             totalSum += (gas[i]-cost[i]);
             
         // we need summation of gas nd cost, to check if gas sum is less than cost   sum, it shows we can't reach to our destination.
