12cb0c584172c16e93c49a26cc89c24f7027028e
nsk-here <110277686+nsk-here@users.noreply.github.com>
2022-09-28 22:06:30 +0530

    wrote Comment

--- array/1207. Unique Number of Occurrences/code.java
+++ array/1207. Unique Number of Occurrences/code.java
@@ -1,16 +1,19 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) 
     {
-      
+      // Count no. of value in map along with value
       Map<Integer, Integer> freq = new HashMap<>();
       for(int val : arr)
         freq.put(val,freq.getOrDefault(val,0)+1);
       
       Set<Integer> map = new HashSet<>();
       
+      
       for(Map.Entry<Integer,Integer> mapEnt : freq.entrySet() )
       {
         int val = mapEnt.getValue();
+          
+        // check it if it has repeating value than set will return false    
         if(map.contains(val))
            return false;
         map.add(val);
