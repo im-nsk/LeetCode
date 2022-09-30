d48b0f72cac25d2b578cc668533f3db3346d5dab
nsk-here <110277686+nsk-here@users.noreply.github.com>
2022-09-30 20:31:07 +0530

    Update code.java

--- array/11. Container With Most Water/code.java
+++ array/11. Container With Most Water/code.java
@@ -1,14 +1,18 @@
 class Solution {
     public int maxArea(int[] height) 
     {
+     // start with first and last:   
       int left = 0;
       int right = height.length-1;
       int area = 0;
       
+      // traversal till left is smaller than right:  
       while(left < right)
       {
+       // get the area by multiple of min height[left], height[right] and right-left:   
         area = Math.max(area, Math.min(height[left], height[right])*(right-left));
-          
+        
+         // 
           if(height[left] > height[right])
             right--;
           else
