684b0cd7a080e83a2911ff00398e8a165a766898
nsk-here <110277686+nsk-here@users.noreply.github.com>
2022-10-08 22:35:53 +0530

    Update code.java

--- array/16.3sum closest/code.java
+++ array/16.3sum closest/code.java
@@ -1,16 +1,24 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
+        // add the summation of first, second nd last....
         int result = nums[0] + nums[1] + nums[nums.length-1];
+        // sort the array:
         Arrays.sort(nums);
         
+        // traverse the array len-2:
         for(int i = 0; i < nums.length-2; i++){
             
+            // j value should be i+1:
             int j = i+1;
+            // k value should be last index:
             int k = nums.length-1;
             
+            // iterate the loop, till j should be less than k:
             while(j < k){
+                // add the summation of first, second nd last....
                 int currSum = nums[i] + nums[j] + nums[k];
                 
+                // 
                 if(currSum > target)
                     k--;
                 else
