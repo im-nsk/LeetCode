62276758c5841ea7281e5408bd6a0e99cce19d74
nsk-here <110277686+nsk-here@users.noreply.github.com>
2022-10-14 09:34:08 +0530

    Add files via upload

---
+++ linkedList/2095. Delete the Middle Node of a Linked List/code.java
@@ -0,0 +1,21 @@
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+        if(head.next == null) return null;
+        ListNode p = head;
+        int lenLinkedList = 0;
+        while(p != null)
+        {
+            lenLinkedList++;
+            p = p.next;
+        }
+        int mid = lenLinkedList/2;
+        p = head;
+        while(p != null && mid > 1)
+        {
+            p = p.next;
+            mid--;
+        }
+        p.next = p.next.next;
+        return head;
+    }
+}
