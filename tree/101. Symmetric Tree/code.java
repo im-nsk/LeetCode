d054b349edea7373a4eebce7aaf18a76de781399
nsk-here <110277686+nsk-here@users.noreply.github.com>
2022-10-19 04:43:41 +0530

    better one

--- tree/101. Symmetric Tree/code.java
+++ tree/101. Symmetric Tree/code.java
@@ -1,18 +1,13 @@
+https://leetcode.com/problems/symmetric-tree/
+
 class Solution {
-    private boolean checkSym(TreeNode p, TreeNode q)
-    {
-        if(p == null && q == null) return true;
+    private boolean checkLeftRight(TreeNode p, TreeNode q){
+        if(p == null && q == null)return true;
         
-        return p != null && q != null && p.val == q.val && checkSym(p.left, q.right) && checkSym(p.right, q.left);
+        return (p != null && q != null && p.val == q.val && checkLeftRight(p.left, q.right) && checkLeftRight(p.right, q.left));
     }
     public boolean isSymmetric(TreeNode root) {
-        
-        TreeNode p = root.left;
-        TreeNode q = root.right;
-        
-        if(p == null && q == null) return true;
-        
-        return p != null && q != null && p.val == q.val && checkSym(p.left, q.right) && checkSym(p.right, q.left);
+        return checkLeftRight(root.left, root.right);
         
     }
 }
