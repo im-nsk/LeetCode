6e580ce73e62e6f6127794c15e15a2e171f991bb
nsk-here <110277686+nsk-here@users.noreply.github.com>
2022-10-26 15:00:41 +0530

    Add files via upload

---
+++ array/523. Continuous Subarray Sum/code.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        int totSum = 0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(0,-1);
+        
+        for(int i = 0; i < nums.length; i++){
+            totSum += nums[i];
+            int rem = totSum % k;
+            if(!map.containsKey(rem))
+                map.put(rem, i);
+            else if(i-map.get(rem) >1){
+                return true;
+              
+            }
+        }
+        return false;
+    }
+}
